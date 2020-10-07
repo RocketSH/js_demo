d1da3d75b4e42a25b706964eaf5b3ef852c143cc
Shuhan Hu <1119han@gmail.com>
2020-10-08 00:56:53 +0800

    Impelement layout

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
-var author = 'ShuHan';
-var myHeading = document.querySelector('h1');
-myHeading.textContent = `Hello ${author} ;)`;
+let author = 'RocketSH';
+let myHeading = document.querySelector('h1');
+myHeading.textContent = `Hello ${author} `;
+
+let starNumber1 = document.querySelector("#starNumber1");
+let starNumber2 = document.querySelector("#starNumber2");
+let starNumberTotal = (starNumber1 + starNumber2);
+
+ document.querySelector("#starCalculator").addEventListener(
+   "submit",
+   function (event) {
+     alert(starNumberTotal);
+     event.preventDefault();
+   },
+   false
+ );
